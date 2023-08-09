5eaf41cde8a2476b863eeae199e61d10c210faff
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-08-09 19:03:47 +0530

    Add files via upload

---
+++ 1.2 Array Operation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	
+	//////insertion
+//	int size, pos,num ;
+//	int a[]={1,2,4,5,6,7};
+//	size=sizeof(a)/sizeof(int);
+//	cout<<"enter the posn"<<endl;
+//	cin>>pos;
+//	cout<<"enter num"<<endl;
+//	cin>>num;
+//	if(pos<=0 || pos>=size){
+//		cout<<"invalid";
+//	}
+//	else{
+//		for(int i=size-1;i>=pos-1;i--){
+//			a[i+1]=a[i];
+//		}
+//		a[pos-1]=num;
+//		size++;
+//	}
+//	for(int i=0;i<size;i++){
+//	
+//	cout<<a[i]<<" "; 
+//	
+//}
+	
+	return 0;
+}
