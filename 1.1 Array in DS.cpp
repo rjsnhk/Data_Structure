5a161c3a7493a285840ba89ac4eefdedec8bdb2c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-08-09 19:03:32 +0530

    Add files via upload

---
+++ 1.1 Array in DS.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int ar[5]={1,2,3,4,5};
+//	cout<<&ar<<endl;
+//	cout<<&ar[0]<<endl;
+//	cout<<&ar[1]<<endl;
+//	cout<<&ar[2]<<endl;
+//	cout<<(&ar)+8 <<endl ;
+//	cout<<&ar[3]<<endl;
+//	cout<<&ar[1]<<endl;
+
+//int rn[6];
+//
+//for(int i=0;i<6;i++){
+//	
+//	cin>>ar[i];
+//	
+//}
+//for(int i=0;i<6;i++){
+//	
+//	cout<<ar[i]<<" "; 
+//	
+//}
+	
+	cout<<sizeof(ar)/sizeof(int);
+	return 0;
+}
